1660b6a5ea4c244a6f5cd19e531df605f5981b63
成在協 <sunny@seizaikyounoMacBook-Pro.local>
2019-12-10 02:19:51 +0900

    [Start]3-8

---
+++ three.rb
@@ -0,0 +1,20 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか？"
+a = gets.to_i
+i = 1
+while  i <= a do
+puts "#{i}回目の計算"
+puts "２つの値を入力してください"
+b = gets.to_i
+c = gets.to_i
+puts "b = #{b}"
+puts "c = #{c}"
+puts "計算結果を出力します"
+puts "a + b = #{a + b}"
+puts "a - b = #{a - b}"
+puts "a * b = #{a * b}"
+puts "a / b = #{a / b}"
+i += 1
+end
+
+puts "計算を終了します"
